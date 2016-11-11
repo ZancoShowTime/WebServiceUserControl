fb7e2758bdb9d996e4f232a5d5203ee8db8bac5c
ZancoShowTime <zancoshowtime@gmail.com>
2016-11-11 20:35:25 -0200

    Criacao do projeto

---
+++ src/main/java/modelo/Usuario.java
@@ -0,0 +1,50 @@
+package modelo;
+
+import java.util.Date;
+
+public class Usuario {
+	
+	private Long id;
+	private String nome;
+	private String senha;
+	private String email;
+	private String cpf;
+	private Date dataNascimento;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+}
